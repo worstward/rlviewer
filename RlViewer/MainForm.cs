fd45b7571b8d67e93d8df5e93a29e1b3c359cd15
ww_ho <lexxlex@mail.ru>
2016-02-03 18:00:13 +0300

    changed main form

---
+++ RlViewer/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RlViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            using (var openFileDlg = new OpenFileDialog() { Filter = Resourses.Filter })
+            {
+                if (openFileDlg.ShowDialog() == DialogResult.OK)
+                {
+                    Hierarchy.FileProperties properties = new Hierarchy.FileProperties(openFileDlg.FileName);
+
+                    var file = Factories.FileFactory.GetFactory(properties).Create(properties);
+                }
+            }
+
+
+            InitializeComponent();
+        }
+    }
+}
